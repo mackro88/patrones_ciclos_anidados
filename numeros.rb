a2fb9bee46d696ead40a74602af114a906ade796
Romina Pineda <romi.macka@gmail.com>
2020-10-16 10:14:21 -0300

    UPDATE patrones.rb

--- numeros.rb
+++ numeros.rb
@@ -1,3 +1,6 @@
+## Escribir un programa llamado numeros.rb , que reciba por linea de comandos la cantidad de lineas, y dibuje el siguiente patrón: 1 12 123 1234 12345
+## ruby numeros.rb 5
+
 lineas = ARGV[0].to_i
 i = 1
 
--- patrones.rb
+++ patrones.rb
@@ -16,11 +16,20 @@ def letra_i(n)
     blank = (n-1)/2
     n.times{|i| print "*"}
     print "\n"
-    for x in 2..n do
-        blank.times{|i| print " "}
-        print "*"
-        blank.times{|i| print " "}
-        print "\n"
+    if n.odd?
+        for x in 2..n do
+            blank.times{|i| print " "}
+            print "*"
+            blank.times{|i| print " "}
+            print "\n"
+        end
+    else
+        for x in 2..n do
+            blank.times{|i| print " "}
+            print "**"
+            blank.times{|i| print " "}
+            print "\n"
+        end
     end
     n.times{|i| print "*"} 
 end
@@ -38,6 +47,43 @@ def letra_z(n)
 end
 
 def letra_x(n)
+    inner = n-2
+    b = n/2
+    l = 1
+    w = n-3
+    print "*"
+    inner.times{|i| print " "}
+    print "*\n"
+    inner.times do
+        if b > 1 
+            l.times{|x| print " "}
+            print "*" 
+            w -= 1   
+            w.times{|x| print " "}
+            print "*\n"
+            b -= 1
+            l += 1 
+            w -= 1
+        elsif b < 1
+            l.times{|x| print " "}
+            print "*"
+            w.times{|x| print " "}
+            print "*\n"
+            l -= 1
+            w += 2 
+        else b = 1  
+            l.times{|x| print " "}
+            print "*\n"
+            b -= 1
+            l -= 1
+            w += 1
+        end
+    end
+    print "*"
+    inner.times{|i| print " "}
+    print "*\n"
+
+
 end
 
 def numero_cero(n)
@@ -46,14 +92,36 @@ end
 def navidad(n)
 end
 
-letra_o(n)
-print "\n"
-letra_i(n)    
-print "\n"
-letra_z(n)
-print "\n"
-letra_x(n)
-print "\n"
+if n >= 3
+    letra_o(n)
+    print "\n" 
+else
+    print "El número debe ser mayor o igual a 3 para generar la forma O \n"
+end
+if n >= 3
+    letra_i(n)    
+    print "\n"
+else
+    print "El número debe ser mayor o igual a 3 para generar la forma I \n"
+end
+if n > 3
+    letra_z(n)
+    print "\n"
+else
+    print "El número debe ser mayor a 3 para generar la forma Z \n"
+end
+if n.odd? && n >= 3
+    letra_x(n)
+    print "\n"
+elsif n.even? 
+    print "El número debe ser impar para generar la forma X \n"
+else n < 3 
+    print "El número debe ser mayor o igual a 3 para generar la forma X \n"
+end
+
 numero_cero(n)
 print "\n"
+
+=begin
 navidad(n)
+=end
